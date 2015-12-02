ffbd90deb19d4908c89afc2b07a39c4d16c7724f
Antony Romero Bonilla <antony.romerobonilla@gmail.com>
2015-12-01 20:54:49 -0600

    se agrega js para menu mobile, se agrega footer y se da estilos a botones

--- src-js/script.js
+++ src-js/script.js
@@ -3,17 +3,8 @@ $(document).ready(function(){
 
   (function($) {
 
-    $('#header_icon').click(function(e){
-      e.preventDefault();
-      $('#menu').removeClass('with--sidebar');
-      $('#header_icon').toggleClass('with--sidebar');
-      $('#header_icon').removeClass('header_icon');
-    });
-
-    $('#close').click(function(e){
-      $('#menu').toggleClass('with--sidebar');
-      $('#header_icon').removeClass('with--sidebar');
-      $('#header_icon').toggleClass('header_icon');
+    $('#close').click(function(){
+      $('#menu').toggleClass('expanded');
     });
 
   })(jQuery);
